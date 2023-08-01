467778d82e33fd1d4b8ca74a655807f61300a544
denbarabraza <denis.bareischev@gmail.com>
2023-08-02 00:39:54 +0300

    feat: initialization, configuration of configs

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
+
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), babel()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
